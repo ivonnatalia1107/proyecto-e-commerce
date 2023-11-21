f7662fc87bb889559d4d012e55327b6aa92816f0
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-11-20 23:09:02 -0400

    ruta individual

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import './detail.css'
 
 export const ProductDetail = () => {
-
     const { id } = useParams();
+    const [product, setProduct] = useState(null);
 
-    const [products, setProducts] = useState([])
-
-    const getProducts = async () => {
-        const resp = await axios.get('https://backend-w1wu.onrender.com/v1/product')
-        console.log(resp.data)
-        setProducts(resp.data.detail)
-    }
     useEffect(() => {
-        getProducts()
-    }, [])
+        const getProducts = async () => {
+            try {
+                const resp = await axios.get('https://backend-w1wu.onrender.com/v1/product');
+                const productData = resp.data.detail;
 
+
+                const selectedProduct = productData.find(product => product._id === id);
+
+                setProduct(selectedProduct);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getProducts();
+    }, [id]);
 
     return (
-        <div>
-            <h2>Product Detail</h2>
-            <p>Product ID: {id}</p>
-        </div>
+        <>
+        <section className='detailRoom'>
+            <div className='detailCard'>
+                {product && (
+                    <div key={product._id}>
+                        <h1 className='title'>{product.name}</h1>
+                        <img className='imgDetail' src={product.img} alt={product.name} />
+                        <div className='description'>
+                        <p>{product.description}</p>
+                        <p>{product.country}</p>
+                        <p>{product.amount}</p>
+                        </div>
+                    </div>
+                )}
+            </div>
+            </section>
+        </>
     );
 };
-
